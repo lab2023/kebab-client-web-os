8fb3db12bd326120050e8fb911d18bb5c9493928
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2012-02-03 18:13:28 +0200

    Issue #49 was completed.

---
+++ kebab/kernel/component/Launcher.js
@@ -0,0 +1,84 @@
+/**
+ * @class Launcher
+ * @extends Ext.toolbar.Toolbar
+ * @author Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
+ *
+ * Kebab OS Kernel Desktop launcher component
+ */
+Ext.define('Kebab.kernel.component.Launcher', {
+    alternateClassName: 'Kebab.Launcher',
+    extend: 'Ext.container.Container',
+    alias: 'widget.kebab_launcher',
+
+    requires: [
+        'Ext.Img'
+    ],
+
+    baseCls: 'kebab-launcher',
+
+    initComponent: function() {
+        var me = this,
+            imagePath = !me.img
+                ? Kebab.helper.root('apps/' + me.launcher.appId.toLowerCase() + '/resources/launcher.svg')
+                : Kebab.helper.root('resources/images/icons/svg/' + me.img.toLowerCase() + '.svg');
+
+        Ext.apply(me, {
+            cls: me.baseCls,
+            items: Ext.create('Ext.Img', {
+                width: me.size || 48,
+                height: me.size || 48,
+                src: imagePath,
+            })
+        });
+
+        try {
+            if (me.launcher.appId) {
+                Ext.apply(me, {
+                    id:me.launcher.appId.toLowerCase() + '-launcher',
+                    launcher:{
+                        appId: me.launcher.appId
+                    }
+                })
+            }
+        } catch (e) {}
+
+        me.callParent(arguments);
+    },
+
+    listeners: {
+        render: function(cp) {
+            var dockPosition;
+
+            try {
+                dockPosition = cp.ownerCt.dock;
+            } catch (e) {
+                dockPosition: 'left';
+            }
+
+            cp.qtip = Ext.create('Ext.tip.ToolTip', {
+               target: cp.getEl(),
+               showDelay: 75,
+               hideDelay: 75,
+               anchor:  dockPosition == 'left' ? 'right' : 'top',
+               html: cp.tooltip || 'Click to open'
+            });
+
+            cp.getEl().on('click', function(e) {
+                e.stopEvent();
+                cp.fireEvent('click', cp, e);
+            });
+
+            /*
+            cp.getEl().on('mouseover', function(e) {
+                e.stopEvent();
+                cp.getEl().down('img').scale(300,300);
+                cp.fireEvent('mouseover', cp, e);
+            });
+            */
+        }
+    },
+
+    setTooltip: function(text) {
+        this.qtip.update(text);
+    }
+});
