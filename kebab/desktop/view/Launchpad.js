3d4ab623bb615015425ad659f1e14a2a0657243b
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2012-02-03 18:12:24 +0200

    Issue #34 launchpad widget was created. Launchers and departments was added. Filter apps for selected department.

--- kebab/desktop/view/Launchpad.js
+++ kebab/desktop/view/Launchpad.js
@@ -1,24 +1,56 @@
 /**
- * @class ForgotPassword
+ * @class Launchpad
  * @extends Ext.window.Window
  * @author Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
  *
  * Kebab OS Desktop launchpad widget
  */
 Ext.define('Kebab.desktop.view.Launchpad', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.window.Window',
     alias : 'widget.desktop_launchpad',
 
-    id: 'desktop-index',
+    id: 'desktop-launchpad',
 
     initComponent: function() {
         var me = this;
 
+        var bodyEl = Ext.fly('desktop-index-body');
+
         Ext.apply(me, {
             title: 'Launchpad',
-            frame:true,
-            layout: 'fit',
-            renderTo: 'desktop-index-body'
+            border:false,
+            bodyStyle: 'background: transparent',
+            preventHeader : true,
+            width: 550,
+            height: 500,
+            autoShow: true,
+            shadow: false,
+            closeAction: 'hide',
+            layout: {
+                type: 'hbox',
+                pack: 'start',
+                align: 'stretch'
+            },
+            bbar: ['->',{
+                iconCls: 'icon-arrow_out',
+                enableToggle: true,
+                handler: function(btn) {
+                    if (!me.maximized == true) {
+                        btn.setIconCls('icon-arrow_in');
+                        me.maximize();
+                    } else {
+                        btn.setIconCls('icon-arrow_out');
+                        me.restore();
+                    }
+                }
+            }],
+            items: [{
+                flex: 1,
+                xtype: 'desktop_launchpad_launchers'
+            },{
+                width: 200,
+                xtype: 'desktop_launchpad_departments'
+            }]
         }, null);
 
         me.callParent(arguments);
